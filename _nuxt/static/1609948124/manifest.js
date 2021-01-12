5f202f2f72c57f70d2bb57ae5cba7b927354f447
Rosario Terranova <rosarioterranova91@gmail.com>
2021-01-12 11:54:53 +0100

    added analytics

--- _nuxt/static/1609948124/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fgames","\u002Fweb","\u002F","\u002Fmobile"]})
---
+++ _nuxt/static/1610448805/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fgames","\u002Fmobile","\u002Fweb"]})
