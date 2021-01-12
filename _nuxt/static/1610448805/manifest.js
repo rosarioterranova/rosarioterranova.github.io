86eb648e7459075462871b05a9874787a6a313b1
Rosario Terranova <rosarioterranova91@gmail.com>
2021-01-12 15:00:20 +0100

    updated

--- _nuxt/static/1610448805/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fgames","\u002Fmobile","\u002Fweb"]})
---
+++ _nuxt/static/1610459966/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fgames","\u002F","\u002Fweb","\u002Fmobile"]})
