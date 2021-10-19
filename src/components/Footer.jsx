07e4e89b8a305ab6f20c880e2b9915480f51cbbd
Rosario Terranova <rosarioterranova91@gmail.com>
2021-10-19 21:25:59 +0200

    update

--- src/components/Footer.jsx
+++
@@ -1,6 +0,0 @@
-export default function Footer(){
-    return <footer className="text-dark text-center pb-2">
-        <hr />
-        Website made by Rosario Terranova with React.js
-    </footer>
-}
